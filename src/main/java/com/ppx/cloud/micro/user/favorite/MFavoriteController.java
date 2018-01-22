51f71e391a265c0cb36f9f2911f6ae85d9868707
dengxz <13602493280@qq.com>
2018-01-22 19:16:14 +0800

    t

--- src/main/java/com/ppx/cloud/micro/user/favorite/MFavoriteController.java
+++ src/main/java/com/ppx/cloud/micro/user/favorite/MFavoriteController.java
@@ -2,21 +2,30 @@ package com.ppx.cloud.micro.user.favorite;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ppx.cloud.common.controller.ControllerReturn;
+import com.ppx.cloud.micro.user.cart.MCartService;
 
 
 @Controller	
 public class MFavoriteController {
 	
-
 	
-	@GetMapping @ResponseBody
-	public Map<String, Object> test() {
-		System.out.println("xxxxxxxxxxxxxout:------------------0001");
+	@Autowired
+	private MFavoriteService serv;
+	
+	@PostMapping @ResponseBody
+	public Map<String, Object> addProduct(@RequestParam Integer prodId) {
+		
+		
+		int r = serv.addProduct(prodId);
+		
 		
 		return ControllerReturn.ok();
 	}
--- src/main/java/com/ppx/cloud/micro/user/favorite/MFavoriteService.java
+++ src/main/java/com/ppx/cloud/micro/user/favorite/MFavoriteService.java
@@ -1,20 +1,49 @@
 package com.ppx.cloud.micro.user.favorite;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ppx.cloud.common.jdbc.MyDaoSupport;
+import com.ppx.cloud.micro.common.MGrantContext;
+import com.ppx.cloud.storecommon.query.bean.QueryProduct;
+import com.ppx.cloud.storecommon.query.service.QueryCommonService;
 
 
 @Service
 public class MFavoriteService extends MyDaoSupport {
 	
+	@Autowired
+	private QueryCommonService queryServ;
 	
-	public void test() {
+	public int addProduct(@RequestParam Integer prodId) {
+		String openid = MGrantContext.getWxUser().getOpenid();
+		int storeId = MGrantContext.getWxUser().getStoreId();
 		
-		System.out.println("------------------go go go----------------");
+		String sql = "insert into user_favorite(OPENID, PROD_ID, STORE_ID) values(?, ?, ?)";
+		int r = getJdbcTemplate().update(sql, openid, prodId, storeId);
+		
+		return r;
 		
 	}
 	
+	
+	
+	public List<QueryProduct> listProduct() {
+		String openid = MGrantContext.getWxUser().getOpenid();
+		int storeId = MGrantContext.getWxUser().getStoreId();
+		
+		String sql = "select PROD_ID from user_favorite where OPENID = ? and STORE_ID = ?";
+		List<Integer> prodIdList = getJdbcTemplate().query(sql, BeanPropertyRowMapper.newInstance(Integer.class), openid, storeId);
+		
+		List<QueryProduct> list = queryServ.listProduct(prodIdList, storeId);
+		
+		
+		return list;
+	}
 	
 	
 	
