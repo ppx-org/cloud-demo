bcaadb10afaf8460438e19570d3dd0a1bcab523b
dengxz <13602493280@qq.com>
2018-03-21 19:23:15 +0800

    t

--- src/main/java/com/ppx/cloud/store/content/home/CleanHomeController.java
+++ src/main/java/com/ppx/cloud/store/content/home/CleanHomeController.java
@@ -1,13 +1,16 @@
 package com.ppx.cloud.store.content.home;
 
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ppx.cloud.common.controller.ControllerReturn;
@@ -18,6 +21,9 @@ import com.ppx.cloud.store.merchant.store.StoreService;
 public class CleanHomeController {
 	
 	@Autowired
+	private WebApplicationContext app;
+	
+	@Autowired
 	private StoreService storeServ;
 
 	@GetMapping
@@ -26,10 +32,46 @@ public class CleanHomeController {
 		mv.addObject("storeList", storeServ.listStore());
 		return mv;
 	}
-
-	//@CacheEvict(value="listHome", key="#storeId")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@PostMapping @ResponseBody
 	public Map<String, Object> cleanListHome(@RequestParam Integer storeId) {
+		StringRedisTemplate stringRedisTemplate = app.getBean(StringRedisTemplate.class);
+		
+		stringRedisTemplate.delete("listHome::" + storeId);
+		
+		Set<String> keys = stringRedisTemplate.keys("levelProd::" + storeId + "::*");
+		stringRedisTemplate.delete(keys);
+	
 		return ControllerReturn.ok(1);
 	}
 }
