4f905c12ddd8702f5ae2bef39f161ef1d26a363a
dengxz <13602493280@qq.com>
2018-04-17 18:33:14 +0800

    t

--- src/main/java/com/ppx/cloud/demo/module/test/Test.java
+++ src/main/java/com/ppx/cloud/demo/module/test/Test.java
@@ -1,37 +1,23 @@
 package com.ppx.cloud.demo.module.test;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import net.coobird.thumbnailator.Thumbnails;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("9999999999999999999");
 		
-		//RestTemplate restTemplate = new RestTemplate(new SimpleClientHttpRequestFactory() { protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException { super.prepareConnection(connection, httpMethod); connection.setInstanceFollowRedirects(false); } });
-
-		RestTemplate restTemplate = RestTemplateUtils.getRestTemplate();
-
-		HttpHeaders headers = new HttpHeaders();
-        headers.add("Connection", "keep-alive");
-        headers.add("Cache-Control", "max-age=0");
-        headers.add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
-        headers.add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-        headers.add("Accept-Encoding", "gzip, deflate");
-        headers.add("Accept-Language", "zh-CN,zh;q=0.8");
-        headers.add("Content-Type", "application/x-www-form-urlencoded");
-        headers.add("Cookie", "");
-	       
-        //HttpEntity<String> formEntity = new HttpEntity<String>("os_username=hukezhou&os_password=hukezhou&os_destination=&user_role=&atl_token=&login=登录", headers); 
-        HttpEntity<String> formEntity = new HttpEntity<String>("os_username=dengxiangzhongs&os_password=dengxiangzhong&os_destination=%2Fbrowse%2FXY-62&user_role=&atl_token=&login=%E7%99%BB%E5%BD%95", headers);        	
-    	ResponseEntity<String> r = restTemplate.exchange("http://t.51hrc.cn/login.jsp", HttpMethod.POST, formEntity, String.class, "");
-    	
-    	System.out.println("r:" + r.getStatusCode());
-    	System.out.println("r:" + r.getHeaders());
-    	System.out.println("r:" + r.getBody());
+		System.out.println("xxxxxxxxx--1");
+		
+		String path = "C:/Users/LENOVO/Desktop/tmp/1.png";
+		Thumbnails.of(path).size(150, 150).toFile("C:/Users/LENOVO/Desktop/tmp/1_150.png");
+		
+		
+		/*
+		Thumbnails.of(targetImg)  
+        .watermark(position, ImageIO.read(new File(pressImg)), alpha)  
+        .outputQuality(1)//生成质量100%  
+        .scale(1)//缩放比例  
+        .toFile(targetImg); */
 	}
 
 }
