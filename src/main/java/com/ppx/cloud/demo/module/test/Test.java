de0fbf3e1431d53e0f258946d474794c6e8eee2a
dengxz <13602493280@qq.com>
2018-04-18 18:04:12 +0800

    t

--- src/main/java/com/ppx/cloud/demo/module/test/Test.java
+++ src/main/java/com/ppx/cloud/demo/module/test/Test.java
@@ -1,48 +1,68 @@
 package com.ppx.cloud.demo.module.test;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Coordinate;
-import net.coobird.thumbnailator.geometry.Positions;
 
 public class Test {
-
-	public static void main(String[] args) throws Exception {
-		
-		System.out.println("xxxxxxxxx--1");
-		
-		
-		//String path = "C:/Users/LENOVO/Desktop/tmp/1.png";
-		//Thumbnails.of(path).size(150, 150).toFile("C:/Users/LENOVO/Desktop/tmp/1_150.png");
-		
-		
-		String targetImg = "E:/U/img/blank500.png";
-		
-		String pressImg = "E:/U/img/1_100.png";
-		
 	
-		String toImg = "E:/U/img/blank500_new001.png";
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println("----------begin");
+		
+		String path = "E:/U/img/";
+		
+		
+		String toImgFile = "blank500_out.png";
+		
+		Map<String, String> map = new HashMap<String, String>();
+		
+		// 公式:=CONCATENATE("map.put(""",B2,""",""",C2,D2,""");")
+		map.put("存过0","00");
+		map.put("存过1","10");
+		map.put("存过2","20");
+		map.put("存过3","30");
+		map.put("存过4","40");
+		
+		for (String key : map.keySet()) {
+			String pressImg = path + key + ".png";
+			int x = (map.get(key).charAt(0) - 48) * 100;
+			int y = (map.get(key).charAt(1) - 48) * 100;
 			
-		
-		
-		
-		
-		
-		
-		
-		
-		Coordinate p = new Coordinate(0, 0);
-		
-		Thumbnails.of(targetImg)  
-        .watermark(p, ImageIO.read(new File(pressImg)), 1)  
-        .outputQuality(1) // 生成质量100%  
-        .scale(1) // 缩放比例 
-        .toFile(toImg);
-		
-		
+			String toImg = path + toImgFile;
+			String targetImg = toImg;
+			
+			File f = new File(toImg);
+			if (!f.exists()) {
+				targetImg = path + "blank500.png";
+			}
+			
+			System.out.println("out:" + targetImg);
+			Coordinate c = new Coordinate(x, y);
+			Thumbnails.of(targetImg)  
+	        	.watermark(c, ImageIO.read(new File(pressImg)), 1)  
+	        	.outputQuality(1).scale(1).toFile(toImg);
+		}
+			
+		System.out.println("----------end");	
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
