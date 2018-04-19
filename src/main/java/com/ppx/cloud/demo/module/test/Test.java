7fe4cd08cc11cafeedf7afcb7ba70441074a0ab5
dengxz <13602493280@qq.com>
2018-04-19 20:19:29 +0800

    t

--- src/main/java/com/ppx/cloud/demo/module/test/Test.java
+++ src/main/java/com/ppx/cloud/demo/module/test/Test.java
@@ -15,7 +15,7 @@ public class Test {
 	public static void main(String[] args) {
 		
 		// aspose words for java 16.8.0
-		
+		// https://artifact.aspose.com/webapp/#/artifacts/browse/tree/General/repo/com/aspose/aspose-words/18.4/aspose-words-18.4-jdk16.jar
 		
 		String s = "";
 		System.out.println("xxxxxxxxxx:" + s.split(";")[0]);
