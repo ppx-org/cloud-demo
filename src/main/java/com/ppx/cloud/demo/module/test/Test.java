efa36d1b2935bb105978b7ec1ada138f6db1b2d5
dengxz <13602493280@qq.com>
2018-06-06 20:11:08 +0800

    t

--- src/main/java/com/ppx/cloud/demo/module/test/Test.java
+++ src/main/java/com/ppx/cloud/demo/module/test/Test.java
@@ -1,13 +1,12 @@
 package com.ppx.cloud.demo.module.test;
 
 public class Test {
-	
-	
-	public static void main(String[] args) {
+
+	public static void mainX(String[] args) {
 		System.out.println("-----------1");
 		try {
 			Del.test();
-			//throw new NoClassDefFoundError("e");
+			throw new NoClassDefFoundError("e");
 		} catch (Exception e) {
 			System.out.println("-----------2");
 		} finally {
@@ -15,14 +14,20 @@ public class Test {
 		}
 		System.out.println("-----------4");
 	}
-	
-	
 
+	public static void main(String[] args) {
+		// 0 == ''; true
+		/*
+			var orderNo = 201808083343412459;
+			alert(orderNo == 201808083343412456);
+		 */
+		Integer i1 = 129;
+		Integer i2 = 129;
+		System.out.println("out:" + (i1 == 129));
+		
+		
+		System.out.println("--------------01--------------02--------------03--------------ddddddddddddddddddddddddddd");
+		
+	}
 
 }
-
-
-
-
-
-
